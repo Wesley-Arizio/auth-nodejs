a9396cb6ca11cc616dc9deff56091e994b897b01
wesley arizio <wesleyarizio26@gmail.com>
2025-03-02 13:04:17 -0300

    refactor: create base error class

---
+++ src/error.js
@@ -0,0 +1,21 @@
+export class ServiceError extends Error {
+    status;
+    constructor(message, status) {
+        super(message);
+        this.status = status
+    }   
+}
+
+export class ValidationError extends ServiceError {
+    constructor(msg) {
+        super(msg);
+        this.status = 400
+    }
+}
+
+export class InvalidCredentials extends ServiceError {
+    constructor() {
+        super("Invalid Credentials");
+        this.status = 401;
+    }
+}
